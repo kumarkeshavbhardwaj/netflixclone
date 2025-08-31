9027b3e85720418541610cc2016e191054b4346e
kumar <kumarkeshavbhardwaj@gmail.com>
2025-08-31 19:26:13 +0530

    Integrated the API and designed the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import Navbar from './components/Navbar'
+import { Route, Routes } from "react-router"
+import Home from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
    <>
-   <h1 >
-    Hero
-   </h1>
+   
+    <Navbar/>
+ <Routes>
+<Route path='/' element={<Home/>}></Route>
+    </Routes>
    </>
   )
 }

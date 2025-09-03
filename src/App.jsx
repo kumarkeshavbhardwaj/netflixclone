b1b063dde0cf4317861c5bd374a1ae9107cf59ac
kumar <kumarkeshavbhardwaj@gmail.com>
2025-09-03 22:25:19 +0530

    Implemented firebase into app

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@
 import Navbar from './components/Navbar'
 import { Route, Routes } from "react-router"
 import Home from './components/Home'
+import { AuthContextProvider } from './components/context/AuthContext'
 
 function App() {
 
 
   return (
    <>
-   
-    <Navbar/>
+   <AuthContextProvider>
+ <Navbar/>
  <Routes>
 <Route path='/' element={<Home/>}></Route>
     </Routes>
+   </AuthContextProvider>
+   
    </>
   )
 }

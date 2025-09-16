a7857b5bfc9fde0c036c300f67a20f52f4b79bd5
kumar <kumarkeshavbhardwaj@gmail.com>
2025-09-16 21:23:36 +0530

    Firestore added

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -1,17 +1,19 @@
 import {createContext, useContext, useEffect, useState} from "react";
-import {auth} from '../../firebase'
+import {auth, db} from '../../firebase'
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged} from 'firebase/auth'
+import {setDoc, doc} from 'firebase/firestore'
 
 const AuthContext = createContext();
 
 export function AuthContextProvider({children}) {
     const [user, setUser] = useState(null);
 
-    function signUp(email, password) {
-        console.log('signUp running of context')
-        return createUserWithEmailAndPassword(auth, email, password).then((e)=>{
-            console.log(e.user)
-        });
+    function signUp (email, password) {
+
+         createUserWithEmailAndPassword(auth, email, password)
+         setDoc(doc(db, 'users', email), {
+            'savedShows' : []
+         })
 
     }
 
